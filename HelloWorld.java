5176da797e4c4a2557e194192536e4739b999a7b
Divyam Upadhyay <37511980+divyam7788@users.noreply.github.com>
2020-12-29 15:16:09 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld!!!"); 
+        System.out.println("HelloWorld!!!");
+        System.out.println("covid19attack!");        
     }
 }
